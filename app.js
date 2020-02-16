96c5a0369b314c2a8b4e126d77a007bde48f2ec0
Shawn Wilson <wilson.ohyeah@gmail.com>
2020-02-16 14:05:38 -0700

    Gets json-api installed, this lib is kinda amazing! Getting valid jsonapi responses back now with all of our fixture data that was uploaded to the db.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
+const api = require('json-api');
 
 const app = express();
 
@@ -21,9 +22,6 @@ app.use(cors({ origin: 'http://localhost:7000' }));
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 
-// DATABASE
-mongoose.connect('mongodb://gordon:rosemary-awkward-worcestershire1@ds119748.mlab.com:19748/mcc-development', { useNewUrlParser: true });
-
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -32,19 +30,59 @@ db.once('open', function () {
 });
 
 // END POINTS
-const indexRouter = require('./app/routes/index');
-const seasonsRouter = require('./app/routes/seasons');
-const episodesRouter = require('./app/routes/episodes');
-const contestantsRouter = require('./app/routes/contestants');
-const participantsRouter = require('./app/routes/participants');
-const judgesRouter = require('./app/routes/judges');
 
-app.use('/', indexRouter);
-app.use('/seasons', seasonsRouter);
-app.use('/episodes', episodesRouter);
-app.use('/contestants', contestantsRouter);
-app.use('/participants', participantsRouter);
-app.use('/judges', judgesRouter);
+const models = {
+  "Season": require('./app/models/season'),
+  "Episode": require('./app/models/episode'),
+  "Challenge": require('./app/models/challenge'),
+  "Contestant": require('./app/models/contestant'),
+  "Participant": require('./app/models/participant'),
+  "Judge": require('./app/models/judge'),
+};
+
+const adapter = new api.dbAdapters.Mongoose(models);
+const registry = new api.ResourceTypeRegistry({
+  "seasons": {},
+  "episodes": {},
+  "challenges": {},
+  "contestants": {},
+  "participants": {},
+  "judges": {},
+}, {
+  "dbAdapter": adapter,
+  "urlTemplates": {
+    "self": "/{type}/{id}"
+  }
+});
+
+const opts = { host: 'localhost:3000' };
+
+// Set up a front controller, passing it controllers that'll be used
+// to handle requests for API resources and for the auto-generated docs.
+var Front = new api.httpStrategies.Express(
+  new api.controllers.API(registry),
+  new api.controllers.Documentation(registry, { name: 'MCC-API' })
+);
+
+app.get("/", Front.docsRequest);
+
+app.get("/:type(seasons)", Front.apiRequest);
+app.get("/:type(season|seasons)/:id", Front.apiRequest);
+
+app.get("/:type(episodes)", Front.apiRequest);
+app.get("/:type(episode|episodes)/:id", Front.apiRequest);
+
+app.get("/:type(challenges)", Front.apiRequest);
+app.get("/:type(challenge|challenges)/:id", Front.apiRequest);
+
+app.get("/:type(contestants)", Front.apiRequest);
+app.get("/:type(contestant|contestants)/:id", Front.apiRequest);
+
+app.get("/:type(participants)", Front.apiRequest);
+app.get("/:type(participant|participants)/:id", Front.apiRequest);
+
+app.get("/:type(judges)", Front.apiRequest);
+app.get("/:type(judge|judges)/:id", Front.apiRequest);
 
 // API ERROR HANDLING
 app.use(function (req, res, next) {
--- app/models/challenge.js
+++ app/models/challenge.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const challengeSchema = new Schema({
+  id: String,
+  type: String,
+  episode_id: String,
+  participant_ids: Array,
+})
+
+const Challenge = mongoose.model('Challenge', challengeSchema);
+
+module.exports = Challenge;
