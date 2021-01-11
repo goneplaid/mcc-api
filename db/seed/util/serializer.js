61ca65a11141f6e49ece9ac8f2dfe8e0ff5c105e
Shawn Wilson <wilson.ohyeah@gmail.com>
2021-01-10 17:06:02 -0700

    The serializer was broken and I didn't like the dumb API that I initially created for it, so I updated it, made it a little more simple (use an array instead of props w/ indexes that must be defined) and wrote a test for it.

--- db/seed/util/serializer.js
+++ db/seed/util/serializer.js
@@ -1,8 +1,22 @@
+/*
+  Serializer.js
+
+  This simple class is meant to be used in conjunction with the Reader
+  class to proceess and hydrate simple POJO objects from array data.
+
+  Consumers of this class simply need to setup an array of sequential
+  property names that correspond to the order of fields in data arrays
+  passed to this class.
+
+  The serialize() fuction must be overridden. Consumers must define what
+  the shape of the final serialized object should look like.
+*/
+
 class Serializer {
-  constructor(data, keyMap) {
-    Object.keys(keyMap).forEach((key) => {
-      this[key] = data[keyMap[key]];
-    })
+  constructor(data, keys) {
+    for (let key of keys) {
+      this[key] = data[keys.indexOf(key)];
+    }
   }
 
   serialize() {
--- db/tests/challenges-test.js
+++ db/tests/challenges-test.js
@@ -39,41 +39,41 @@ const seasons = selectedSeasonArg ? [Number(selectedSeasonArg.split('=')[1])] : 
 const toLines = (data) => data.toString().trim().split('\n');
 const toCsv = (data) => csvParseSync(file, parserOptions);
 
-describe('Describe challenge data', function() {
+describe('Describe challenge data', function () {
   seasons.forEach((season) => {
-    describe(`for season ${season}:`, function() {
-      beforeEach(function() {
+    describe(`for season ${season}:`, function () {
+      beforeEach(function () {
         this.filePath = path.join(__dirname, `../csv/challenges/season-${season}.csv`);
       });
 
-      it('should contain data', function(done) {
-        fs.readFile(this.filePath, function(error, data) {
+      it('should contain data', function (done) {
+        fs.readFile(this.filePath, function (error, data) {
           assert.ok(data && data.length, 'No data was found, wtf!');
 
           done();
         });
       });
 
-      it('should contain one entry (line) per contestant', function(done) {
-        fs.readFile(this.filePath, function(error, challengeData) {
-          fs.readFile(path.join(__dirname, `../csv/contestants/season-${season}.csv`), function(error, contestantData) {
+      it('should contain one entry (line) per contestant', function (done) {
+        fs.readFile(this.filePath, function (error, challengeData) {
+          fs.readFile(path.join(__dirname, `../csv/contestants/season-${season}.csv`), function (error, contestantData) {
             const challengeLines = toLines(challengeData);
             const contestantLines = toLines(contestantData);
             const message = challengeLines.length > contestantLines.length + 1 ? 'Too many entries' : 'Too few entries';
 
-            assert.equal(challengeLines.length, contestantLines.length + 1, message);
+            assert.strictEqual(challengeLines.length, contestantLines.length + 1, message);
 
             done();
           });
         });
       });
 
-      it('should contain a header with items delimited by a colon (:)', function(done) {
-        fs.readFile(this.filePath, function(error, data) {
+      it('should contain a header with items delimited by a colon (:)', function (done) {
+        fs.readFile(this.filePath, function (error, data) {
           const headers = toLines(data)[0].split(':');
 
-          assert.equal(headers[0], 'Place', 'The first header should be "Place"');
-          assert.equal(headers[1], 'Contestant', 'The second header should be "Contestant"');
+          assert.strictEqual(headers[0], 'Place', 'The first header should be "Place"');
+          assert.strictEqual(headers[1], 'Contestant', 'The second header should be "Contestant"');
 
           let episodeCounter = 1;
 
@@ -88,7 +88,7 @@ describe('Describe challenge data', function() {
               // no-op, everything is just fine, nothing to see here
             } else {
               // we've got a problem here.
-              assert.equal(episodeHeader, episodeCounter, "episodes don't start with 1 or aren't sequential");
+              assert.strictEqual(episodeHeader, episodeCounter, "episodes don't start with 1 or aren't sequential");
             }
           });
 
@@ -96,8 +96,8 @@ describe('Describe challenge data', function() {
         });
       });
 
-      it('should start each line with a contestants finishing place', function(done) {
-        fs.readFile(this.filePath, function(error, data) {
+      it('should start each line with a contestants finishing place', function (done) {
+        fs.readFile(this.filePath, function (error, data) {
           let placeCounter = 1;
 
           toLines(data).splice(1).forEach((line) => {
@@ -107,7 +107,7 @@ describe('Describe challenge data', function() {
               placeCounter += 1;
             } else {
               // we've got a problem here.
-              assert.equal(contestantPlace, placeCounter, "finishing place(s) are missing or aren't sequential'ish");
+              assert.strictEqual(contestantPlace, placeCounter, "finishing place(s) are missing or aren't sequential'ish");
             }
           });
 
@@ -115,11 +115,11 @@ describe('Describe challenge data', function() {
         });
       });
 
-      it('should display a valid contestant name in the second column', function(done) {
-        fs.readFile(this.filePath, function(error, challengeData) {
-          fs.readFile(path.join(__dirname, `../csv/contestants/season-${season}.csv`), function(error, contestantData) {
+      it('should display a valid contestant name in the second column', function (done) {
+        fs.readFile(this.filePath, function (error, challengeData) {
+          fs.readFile(path.join(__dirname, `../csv/contestants/season-${season}.csv`), function (error, contestantData) {
             toLines(challengeData).splice(1).forEach((line, index) => {
-              assert.equal(line.split(':')[1], toLines(contestantData)[index].split(':')[0], "contestant name wasn't found among contestants")
+              assert.strictEqual(line.split(':')[1], toLines(contestantData)[index].split(':')[0], "contestant name wasn't found among contestants")
             });
 
             done();
@@ -127,16 +127,15 @@ describe('Describe challenge data', function() {
         });
       });
 
-      it('every challenge entry must consist of a type and result code', function(done) {
-        fs.readFile(this.filePath, function(error, data) {
+      it('every challenge entry must consist of a type and result code', function (done) {
+        fs.readFile(this.filePath, function (error, data) {
           toLines(data).splice(1).forEach(line => {
             const challenges = line.split(':').splice(2);
 
             challenges.forEach((challenge, index) => {
               const challengeParts = challenge.trim().split('-');
-              const derp = line;
 
-              assert.equal(challengeParts.length, 2, `Invalid challenge parts for line: "${line}", challenge: ${challengeParts}`);
+              assert.strictEqual(challengeParts.length, 2, `Invalid challenge parts for line: "${line}", challenge: ${challengeParts}`);
               assert.ok(challengeTypes.includes(challengeParts[0]), `Challenge type not found: ${challengeParts[0]}`);
               assert.ok(challengeResults.includes(challengeParts[1]), `Challenge result not found: ${challengeParts[1]}`);
             });
